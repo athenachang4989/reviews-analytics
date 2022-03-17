3440b406abe14586ca7f8ca986a30c7c3326f1ef
Athena Chang <j8687julie28@gmail.com>
2022-03-17 17:03:40 +0800

    add another filiter

---
+++ read-3.py
@@ -0,0 +1,30 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line.strip())
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('總共有', len(data), '則留言')
+print('每則留言的平均長度是', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '則留言小於100個字母')
+print(new[0])
+print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '則留言裡有提到good')
+print(good[0])
